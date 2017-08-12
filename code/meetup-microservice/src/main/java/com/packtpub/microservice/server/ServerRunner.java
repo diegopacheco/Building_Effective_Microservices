c9b2014f05af19fa2349a7a2bd8cf01cff1f4ed1
Diego Pacheco <diego.pacheco.it@gmail.com>
2017-08-12 17:48:53 -0300

    Added parameter to enable and disable eureka

--- code/meetup-microservice/src/main/java/com/packtpub/microservice/server/ServerRunner.java
+++ code/meetup-microservice/src/main/java/com/packtpub/microservice/server/ServerRunner.java
@@ -1,20 +1,27 @@
 package com.packtpub.microservice.server;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.inject.Module;
+import com.netflix.config.ConfigurationManager;
 import com.packtpub.microservice.guice.GuiceBindings;
+
+import netflix.karyon.eureka.KaryonEurekaModule;
 
 public class ServerRunner {
 	
 	public static void main(String[] args) {
 		
-		Module[] modules = new Module[]{
-				new GuiceBindings(),
-				//new KaryonEurekaModule() 
-		};
+		List<Module> modules = new ArrayList<>();
+		modules.add(new GuiceBindings());
+
+		if (ConfigurationManager.getConfigInstance().getBoolean("eureka_registry",true))
+			modules.add(new KaryonEurekaModule()); 
 		
 		RxNettyServer server = new RxNettyServer();
 		server.withPort(9090)
-			  .withModules(modules)
+			  .withModules(modules.toArray(new Module[modules.size()]))
 			  .start();
 		
 	}
